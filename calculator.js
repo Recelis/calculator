7edea5ee9aaeb340889fbeadc4b1c634f14dd7b6
JackyRecelis <jackymlui@live.com>
2017-04-26 20:30:50 +1000

    setup html

---
+++ calculator.js
@@ -0,0 +1,22 @@
+/*
+calculator.js
+Jacky Lui
+
+User Story: I can add, subtract, multiply and divide two numbers.
+User Story: I can clear the input field with a clear button.
+User Story: I can keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.
+
+
+functions:
+
+
+input:
+do:
+output:
+
+
+writing in Vanilla JS
+
+do:
+
+*/
